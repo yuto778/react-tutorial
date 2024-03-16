93b0a439edba49a9c76241421def27e1fbe6f2e7
yuto <sakonyuuto@sakonyuutonoMacBook-Air.local>
2024-03-16 12:12:15 +0900

    new

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState, useRef } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+function App() {
+  const [todos,setTodos] = useState([{id:1,name:'Todo1',completed: false}]);
+  
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    //タスクを追加する
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos,{id:uuidv4(), name:name, completed:false}];
+    });
+    todoNameRef.current.value = null;
+
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id );
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  return (
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
+    </div>
+  );
+}
+
+export default App;
